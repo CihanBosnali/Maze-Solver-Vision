816924718ba9a788c41eaa83048d060cb4206195
Cihan Bosnalı <cihanbosna@hotmail.com>
2019-02-07 23:40:34 +0300

    Refactoring example 2

--- EntranceAndExit.py
+++ EntranceAndExit.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np 
 
 def find_entrance_by_color(image, lower_color=np.array([0,100,0]), upper_color=np.array([1,255,1])):
-    mask = cv2.inRange(image, lower_green, upper_green)
+    mask = cv2.inRange(image, lower_color, upper_color)
     return mask
 
     
@@ -10,15 +10,11 @@ def main():
     image = cv2.imread("maze.png")
 
     mask_entrance = find_entrance_by_color(image)
-    
-    lower_red = np.array([0,0,100])
-    upper_red = np.array([1,1,255])
-
-    mask_red = cv2.inRange(image, lower_red, upper_red)
+    mask_exit = find_entrance_by_color(image, lower_color=np.array([0,0,100]), upper_color=np.array([1,1,255]))
 
     while True:
         cv2.imshow("Green", mask_entrance)
-        cv2.imshow("Red", mask_red)
+        cv2.imshow("Red", mask_exit)
 
         if cv2.waitKey() == 27:
             break
